e3de59399d7528137ad30aee7a74fc39a36134fe
edangx202 <129401428+edangx202@users.noreply.github.com>
2023-11-18 02:35:52 +0000

    update week2

--- quote_disp/app.py
+++ quote_disp/app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.route("/health")
 def health():
     return "healthy"
@@ -18,7 +19,8 @@ def home():
 
 @app.route("/get_quote")
 def quote():
-    quote = requests.get("http://quote-gen-container:5000/quote").text
+    # quote = requests.get("http://quote-gen-container:5000/quote").text
+    quote = requests.get("http://web1:5000/quote").text    # see docker-compose.yml
     print("quote - ", quote)
 
     return render_template("quote.html", quote=quote)
